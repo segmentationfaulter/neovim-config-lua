82fd52c6be191fae6ce823c2b9d22ba5f3d2b4a5
Muhammad Saqib <muhammad.saqib@paack.co>
2023-06-03 06:04:52 +0500

    first commit

---
+++ init.lua
@@ -0,0 +1,234 @@
+-- Got help from https://vonheikemen.github.io/devlog/tools/build-your-first-lua-config-for-neovim/
+
+-- Options
+
+vim.g.mapleader = " "
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = 'a'
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = false
+vim.opt.wrap = true
+vim.opt.breakindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.termguicolors = true
+vim.opt.hidden = true
+
+
+
+-- lazy.nvim plugin manager
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	{ 'folke/tokyonight.nvim',
+		{ "catppuccin/nvim",                 name = "catppuccin" },
+		'williamboman/mason.nvim',
+		'williamboman/mason-lspconfig.nvim',
+		'windwp/nvim-autopairs',
+		'neovim/nvim-lspconfig',
+		'hrsh7th/nvim-cmp',
+		'hrsh7th/cmp-nvim-lsp',
+		'hrsh7th/cmp-path',
+		'hrsh7th/cmp-buffer',
+		'lukas-reineke/indent-blankline.nvim',
+		{
+			'nvim-lualine/lualine.nvim',
+			dependencies = { 'nvim-tree/nvim-web-devicons' }
+		},
+		{
+			'nvim-telescope/telescope.nvim',
+			tag = '0.1.1',
+			dependencies = { 'nvim-lua/plenary.nvim' }
+		},
+		{ 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+		'lewis6991/gitsigns.nvim',
+		{ 'akinsho/toggleterm.nvim', version = "*", config = true },
+
+	},
+})
+
+-- Plugins commands
+
+vim.cmd.colorscheme('catppuccin-macchiato')
+require('nvim-autopairs').setup()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require('mason').setup()
+local mason_lspconfig = require("mason-lspconfig")
+mason_lspconfig.setup()
+mason_lspconfig.setup_handlers {
+	function(server_name)
+		require("lspconfig")[server_name].setup { capabilities = capabilities }
+	end,
+}
+require('lualine').setup()
+local telescopeBuiltin = require('telescope.builtin')
+
+require("indent_blankline").setup {}
+
+
+local cmp = require("cmp")
+
+cmp.setup({
+	mapping = cmp.mapping.preset.insert({
+		['<C-u>'] = cmp.mapping.scroll_docs(-4),
+		['<C-d>'] = cmp.mapping.scroll_docs(4),
+		['<C-Space>'] = cmp.mapping.complete(),
+		['<C-e>'] = cmp.mapping.abort(),
+		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = { { name = 'nvim_lsp' }, { name = 'path' }, { name = 'buffer' } }
+})
+
+
+require 'nvim-treesitter.configs'.setup {
+	highlight = { enable = true },
+	indent = { enable = true },
+	ensure_installed = {
+		"bash",
+		"html",
+		"javascript",
+		"elm",
+		"json",
+		"lua",
+		"luadoc",
+		"luap",
+		"markdown",
+		"markdown_inline",
+		"regex",
+		"tsx",
+		"typescript",
+		"vim",
+		"vimdoc",
+		"yaml",
+	},
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<C-space>",
+			node_incremental = "<C-space>",
+			scope_incremental = false,
+			node_decremental = "<bs>",
+		},
+	},
+}
+
+require('toggleterm').setup {
+	open_mapping = [[<c-\>]],
+	direction = 'float'
+}
+
+require('gitsigns').setup {
+	on_attach = function(bufnr)
+		local gs = package.loaded.gitsigns
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map('n', ']c', function()
+			if vim.wo.diff then return ']c' end
+			vim.schedule(function() gs.next_hunk() end)
+			return '<Ignore>'
+		end, { expr = true })
+
+		map('n', '[c', function()
+			if vim.wo.diff then return '[c' end
+			vim.schedule(function() gs.prev_hunk() end)
+			return '<Ignore>'
+		end, { expr = true })
+
+		-- Actions
+		map('n', '<leader>hs', gs.stage_hunk)
+		map('n', '<leader>hr', gs.reset_hunk)
+		map('v', '<leader>hs', function() gs.stage_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+		map('v', '<leader>hr', function() gs.reset_hunk { vim.fn.line("."), vim.fn.line("v") } end)
+		map('n', '<leader>hS', gs.stage_buffer)
+		map('n', '<leader>hu', gs.undo_stage_hunk)
+		map('n', '<leader>hR', gs.reset_buffer)
+		map('n', '<leader>hp', gs.preview_hunk)
+		map('n', '<leader>hb', function() gs.blame_line { full = true } end)
+		map('n', '<leader>tb', gs.toggle_current_line_blame)
+		map('n', '<leader>hd', gs.diffthis)
+		map('n', '<leader>hD', function() gs.diffthis('~') end)
+		map('n', '<leader>td', gs.toggle_deleted)
+
+		-- Text object
+		map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+	end
+}
+
+
+-- My own commands
+
+local saqibGroup = vim.api.nvim_create_augroup('user_cmds', { clear = true })
+vim.api.nvim_create_user_command('ReloadConfig', 'source $MYVIMRC', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = saqibGroup,
+	desc = 'Highlight on yank',
+	callback = function(event)
+		vim.highlight.on_yank({ higroup = 'Visual', timeout = 200 })
+	end
+})
+
+-- Diagnostics config
+
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+for type, icon in pairs(signs) do
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
+
+-- Keybindings
+
+vim.keymap.set({ 'n', 'x' }, 'x', '"_x')
+vim.keymap.set({ 'n', 'x' }, '<leader>w', '<C-w>')
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+vim.keymap.set('n', '<leader>ff', telescopeBuiltin.find_files, {})
+vim.keymap.set('n', '<leader>fg', telescopeBuiltin.live_grep, {})
+
+
+vim.api.nvim_create_autocmd('LspAttach', {
+	group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+	callback = function(ev)
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+		vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+		vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+		vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+		vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set('n', '<leader>wl', function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+		vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+		vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+		vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+		vim.keymap.set('n', '<space>f', function()
+			vim.lsp.buf.format { async = true }
+		end, opts)
+	end,
+})
